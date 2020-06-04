98662aa6f07581526621c4c79394db0bc9b43e48
Louis Robertson <louisrobertson20@gmail.com>
2020-06-04 22:34:35 +0100

    eighth commit

--- app.js
+++ app.js
@@ -493,6 +493,11 @@ app.post("/failure", function(req, res){
   res.redirect("/");
 });
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+
+if (port == null || port ==" " {
+  port = 3000;}
+
+app.listen(port, function() {
+  console.log("Server started successfully");
 });
